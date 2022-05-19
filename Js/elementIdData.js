462a47f1a18d51b8535db62d89ccaa0c225cd451
BidzoLideratori <bidzookro@gmail.com>
2022-05-20 02:17:07 +0400

    master

--- Js/elementIdData.js
+++ Js/elementIdData.js
@@ -2,10 +2,13 @@ export let elem = {
      'beginButton' : document.getElementById('startButtonId'),
      'map' : document.getElementById('mapId'),
      'highScore' : document.getElementById('highScoreId'),
+     'snakeHeadCurrentTop' : '',
+     'snakeHeadCurrentLeft' : '',
      'interval' : '',
      'currentDirection' : 'left',
      'currentChange' : 0,
      'input' : '',
-     'counter' : 0
+     'counter' : 0,
+     'snakeUnits' : [],
 }
 
--- Js/snake.js
+++ Js/snake.js
@@ -5,6 +5,8 @@ export class snake {
     let propertyValue = elem.currentDirection;
     let change = elem.currentChange;
     elem.interval = setInterval(() => {
+      elem.snakeHeadCurrentLeft = getComputedStyle(document.getElementById('snake')).getPropertyValue('left')
+      elem.snakeHeadCurrentTop = getComputedStyle(document.getElementById('snake')).getPropertyValue('top')
       document.addEventListener("keydown", (e) => {
         elem.highScore.innerHTML = `Your Score: ${elem.counter}`;
         elem.input = e.key;
@@ -54,13 +56,8 @@ export class snake {
         computedTop < 0 ||
         computedTop > 580
       ) {
-        if (confirm("you lose")) {
-          document.getElementById("snake").style.top = `${10}px`;
-          document.getElementById("snake").style.left = `${10}px`;
-          location.reload();
-        } else {
-          location.reload();
-        }
+        alert("You Lost");
+        location.reload();
       }
       snake.renderApple(
         parseInt(
@@ -74,11 +71,16 @@ export class snake {
           )
         )
       );
+      this.updateSnakeUnits();
       if (elem.input === "ArrowUp" || elem.input === "ArrowDown") {
         document.getElementById("snake").style.top = `${newPosition}px`;
       } else if (elem.input === "ArrowLeft" || elem.input === "ArrowRight") {
         document.getElementById("snake").style.left = `${newPosition}px`;
       }
+
+      this.updateSnakeUnits();
+
+      this.collisionDetector();
     }, 100);
   }
 
@@ -104,6 +106,53 @@ export class snake {
       document.getElementById("appleId").style.top = `${
         Math.floor(Math.random() * 30) * 20
       }px`;
+      this.renderSnakeUnit()
     }
+
   }
+
+  static renderSnakeUnit() {
+    let snakeElement = document.createElement('div')
+    snakeElement.classList.add('snake-unit')
+    if (elem.snakeUnits.length > 0){
+    snakeElement.style.top = elem.snakeUnits[elem.snakeUnits.length-1].style.top
+    snakeElement.style.left = elem.snakeUnits[elem.snakeUnits.length-1].style.left
+    }
+    else if(elem.snakeUnits.length === 0){
+      snakeElement.style.top = getComputedStyle(document.getElementById('snake')).getPropertyValue('top')
+      snakeElement.style.left = getComputedStyle(document.getElementById('snake')).getPropertyValue('left')
+    }
+    elem.snakeUnits.push(snakeElement);
+    elem.map.appendChild(snakeElement)
+    
+  }
+
+  static updateSnakeUnits() {
+    if(elem.snakeUnits.length > 0){
+    elem.snakeUnits[0].style.top = elem.snakeHeadCurrentTop
+    elem.snakeUnits[0].style.left = elem.snakeHeadCurrentLeft
+ if(elem.snakeUnits.length >= 2){
+    for (let i = elem.snakeUnits.length-1 ; i > 0; i--) {
+      elem.snakeUnits[i].style.top = getComputedStyle(
+        elem.snakeUnits[i - 1]
+      ).getPropertyValue("top");
+      elem.snakeUnits[i].style.left = getComputedStyle(
+        elem.snakeUnits[i - 1]
+      ).getPropertyValue("left");
+        }
+  }
+  }
+}
+
+static collisionDetector(){
+  
+elem.snakeUnits.forEach(element => {
+  if(element.style.top === document.getElementById('snake').style.top && element.style.left === document.getElementById('snake').style.left){
+    alert('You Lose')
+    location.reload();
+  }
+});
+
+
+}
 }
